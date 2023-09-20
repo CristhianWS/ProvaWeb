e2b564597c2823746925709370b03d9f1766ce02
Cristhian Alves <cristhiansilvaalves@gmail.com>
2023-09-20 09:34:14 -0300

    comiit

--- app.js
+++ app.js
@@ -25,6 +25,14 @@ app.post("/cadastrar", function(req, res){
     })
 })
 
+app.get("/consulta", function(req, res){
+    post.findAll().then(function(post){
+        res.render("consulta", {post})
+    }).catch(function(erro){
+        console.log("Erro ao carregar dados do banco: " + erro)
+    })
+})
+
 app.listen(8081, function(){
     console.log("Servidor rodando")
 })
