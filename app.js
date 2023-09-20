f95b5dbf8ad33db5846043ac58c9783cf3468f6a
Cristhian Alves <cristhiansilvaalves@gmail.com>
2023-09-20 09:30:32 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const app = express()
+const handlebars = require("express-handlebars").engine
+const bodyParser = require("body-parser")
+const post = require("./models/post")
+
+app.engine("handlebars", handlebars({defaultLayout: "main"}))
+app.set("view engine", "handlebars")
+
+app.use(bodyParser.urlencoded({extends: false}))
+app.use(bodyParser.json())
+
+app.get("/", function(req, res){
+    res.render("primeira_pagina")
+})
+
+app.post("/cadastrar", function(req, res){
+    post.create({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+    }).then(function(){
+        res.redirect("/")
+    }).catch(function(erro){
+        res.send("Falha ao cadastrar os dados: " + erro)
+    })
+})
+
+app.listen(8081, function(){
+    console.log("Servidor rodando")
+})
